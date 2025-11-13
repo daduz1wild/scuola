faf6fb3dcf735b5decd948bd77cc3be5296edb89
Davide <davidebenedetti1wild@gmail.com>
2025-11-13 08:04:13 +0100

    agg es 13 info

--- 5/INFO/PRATICA/ESERCITAZIONI/XAMPP/es_postback/postback.php
+++ 5/INFO/PRATICA/ESERCITAZIONI/XAMPP/es_postback/postback.php
@@ -1,42 +1,97 @@
 <?php
+// 0 = nessun dato, 1 = elaborazione corretta, 2 = dati non validi
+$caso = 0;
+$err = "";
 
-/*
-Si devono gestire le iscrizioni ai convegni di una manifestazione. Ogni persona che desidera partecipare ad un convegno deve accedere ad una pagina web 
-in cui tramite checkbox seleziona uno o più convegni a cui iscriversi. Il
-form prevede il bottone ISCRIVITI. Realizzare il codice dell’unico script php (uno solo!) che permette sia di selezionare i convegni a cui iscriversi 
-che di vedere il convegno a cui l’utente si è iscritto.
-L’elenco dei convegni previsti è stato precaricato in un array associativo.
-*/
+// Se il form è stato inviato
+if(!empty($_GET)){
 
-    if(empty($_GET))
-        form();
-    else{
-        $convegni=$_GET;
-        foreach($convegni as $key=>$value){
-            
+    // Controllo che esista la chiave 'nome' prima di usarla
+    if(isset($_GET['nome'])){
+        $nome = $_GET['nome'];
 
+        // controllo con la funzione richiesta
+        if(controllo($nome)){
+            $caso = 1; // dati validi → mostra iscrizione
+        } else {
+            $caso = 2; // dati non validi → errore
+            $err = "Errore: il nome non è valido.";
+        }
 
-            
+    } else {
+        // Mancanza del campo 'nome'
+        $caso = 2;
+        $err = "Errore: campo 'nome' non trovato.";
+    }
+}
+
+// funzione di controllo richiesta dal testo
+function controllo($nome){
+    if($nome !== "" )
+        return true;
+    else
+        return false;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Iscrizione ai convegni</title>
+</head>
+<body>
+
+<?php
+// --- CASO 0 o 2: mostra il form ---
+if($caso == 0 || $caso == 2){
+    if($caso == 2)
+        echo "<p style='color:red;'>$err</p>";
+    ?>
+    <form method="get">
+        Nome: <input type="text" name="nome"><br><br>
+
+        <label for="info">Informatica</label>
+        <input type="checkbox" name="info" id="info"><br>
+
+        <label for="tele">Telecomunicazioni</label>
+        <input type="checkbox" name="tele" id="tele"><br>
+
+        <label for="mate">Matematica</label>
+        <input type="checkbox" name="mate" id="mate"><br>
+
+        <label for="film">Film</label>
+        <input type="checkbox" name="film" id="film"><br>
+
+        <label for="macchine">Macchine</label>
+        <input type="checkbox" name="macchine" id="macchine"><br><br>
+
+        <input type="submit" value="ISCRIVITI">
+    </form>
+    <?php
+}
+// --- CASO 1: mostra l’esito dell’iscrizione ---
+else if($caso == 1){
+    $nome = $_GET['nome'];
+    echo "<h2>Iscrizione completata!</h2>";
+    echo "Ciao $nome, ti sei iscritto ai seguenti convegni:<br><ul>";
+
+    $haSelezionato = false;
+    foreach($_GET as $key => $value){
+        if($key != "nome"){
+            echo "<li>$key</li>";
+            $haSelezionato = true;
         }
     }
-        
-    function form(){
-        ?>
-            <form id="form">
-                Nome:<input type="text" name="nome" id="nome">
-                <label for="info">Informatica</label>
-                <input type="checkbox" name="info" id="info">
-                <label for="tele">Telecomunicazioni</label>
-                <input type="checkbox" name="tele" id="tele">
-                <label for="mate">Matematica</label>
-                <input type="checkbox" name="mate" id="mate">
-                <label for="film">Film</label>
-                <input type="checkbox" name="film" id="film">
-                <label for="macchine">Macchine</label>
-                <input type="checkbox" name="macchine" id="macchine">
-                <input type="submit" value="ISCRIVITI">
-            </form>
-        <?php
+
+    if(!$haSelezionato){
+        echo "<li>Nessun convegno selezionato</li>";
     }
 
+    echo "</ul>";
+    echo '<br><a href="http://localhost/progetti/ESE_INFO/es_postback/postback.php">Torna al form</a>';
+}
 ?>
+
+</body>
+</html>
